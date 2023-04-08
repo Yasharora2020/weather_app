c9b4a679ade9924680f94a8eda806e543f10cb92
Yash <yashvardhanarora@gmail.com>
2023-04-08 13:58:25 +1000

    small changes

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import streamlit as st
 import requests
 import os
+import datetime
+import fontawesome as fa
+import streamlit.components.v1 as components
+
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 API_KEY = os.getenv('APP_ID')
+
+sunrise_icon = '<i class="fa-solid fa-sunrise fa-beat-fade"></i>'
+
 
 st.title("Weather App")
 
@@ -19,10 +27,18 @@ data = response.json()
 
 
 if data["cod"] != "404":
-    st.write("Current temperature:", data["main"]["temp"])
-    st.write("Current temperature:", data)
+    st.write("Current temperature:", data['main']['temp'])
+    st.write("Current Wind Speed:", data["wind"]["speed"])
+    st.write("Current Weather:", data["weather"][0]["description"])
+    components.html(sunrise_icon, height=20, width=20)
+    st.write("Sunrise:", datetime.datetime.fromtimestamp(data["sys"]["sunrise"]).strftime('%H:%M'))
+    st.write("Sunset:", datetime.datetime.fromtimestamp(data["sys"]["sunset"]).strftime('%H:%M'))
+    #st.write(f"{fa.icons['sun-o']} Sunrise: {datetime.datetime.fromtimestamp(data['sys']['sunrise']).strftime('%H:%M')}")
 
-   # st.write("Weather description:", data["weather"][0]["description"])
-    #st.write("Wind speed:", data["wind"]["speed"])
+
+    
+    #st.write("Current Weather:", data)
+
+  
 else:
     st.write("City not found")
